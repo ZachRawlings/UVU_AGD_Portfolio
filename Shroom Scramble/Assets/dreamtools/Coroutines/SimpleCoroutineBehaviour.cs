9cb550a19790bee29e599d05bb970e43d1764f44
Zach Rawlings <Zachrg42@gmail.com>
2026-04-03 19:35:40 -0600

    Task: Font, pause button, start scene

--- Shroom Scramble/Assets/dreamtools/Coroutines/SimpleCoroutineBehaviour.cs
+++ Shroom Scramble/Assets/dreamtools/Coroutines/SimpleCoroutineBehaviour.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class SimpleCoroutineBehaviour : MonoBehaviour
 {
     public float seconds = 1;
     private WaitForSeconds _waitForSeconds;
     public UnityEvent @event;
-
+    public UnityEvent @waitEvent;
+    
     private void Awake()
     {
         _waitForSeconds = new WaitForSeconds(seconds);
@@ -19,6 +21,20 @@ public class SimpleCoroutineBehaviour : MonoBehaviour
         {
             yield return _waitForSeconds;
             @event.Invoke();
+        }
+    }
+    
+    public void waitEventInvoke()
+    {
+        StartCoroutine(nameof(waitForSeconds));
+    }
+
+    public IEnumerator waitForSeconds()
+    {
+        while (true)
+        {
+            yield return _waitForSeconds;
+            @waitEvent.Invoke();
         }
     }
 }
